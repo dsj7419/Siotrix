7e63310cbb762b77f0e5275db125527ef446767e
Dan Johnson <dsj7419@gmail.com>
2017-02-03 10:16:09 -0800

    Initial Commit

---
+++ GynBot/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+
+namespace GynBot
+{
+    public class Program
+    {
+        // Convert our sync main to an async main.
+        public static void Main(string[] args) =>
+            new Program().Start().GetAwaiter().GetResult();
+
+        private DiscordSocketClient client;
+        private CommandHandler handler;
+
+        public async Task Start()
+        {
+            // Define the DiscordSocketClient
+            client = new DiscordSocketClient();
+
+            var token = "Mjc1NTIxMjI2OTA0OTYxMDI0.C3TTVw.cVtCtX5Uz8TVTa30fWaMf7yNBEU";
+
+            // Login and connect to Discord.
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.ConnectAsync();
+
+            var map = new DependencyMap();
+            map.Add(client);
+
+            handler = new CommandHandler();
+            await handler.Install(map);
+
+            // Block this program until it is closed.
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
