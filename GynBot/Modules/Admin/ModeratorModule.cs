29139e19d5746edb92b396b628d21db75f8abcd4
Dan Johnson <dsj7419@gmail.com>
2017-02-20 15:33:17 -0800

    FileStruct redo, Added powerdown and getinvite commands

--- GynBot/Modules/Admin/ModeratorModule.cs
+++ GynBot/Modules/Admin/ModeratorModule.cs
@@ -13,17 +13,7 @@ namespace GynBot.Modules.Public
 {
     [Name("Moderator Commands")]
     public class ModeratorModule : ModuleBase<SocketCommandContext>
-    {
-        [Command("leave")]
-        [Remarks("Instructs the bot to leave this Guild.")]
-        [MinPermissions(AccessLevel.ServerAdmin)]
-        public async Task Leave()
-        {
-            if (Context.Guild == null) { await ReplyAsync("This command can only be ran in a server."); return; }
-            await ReplyAsync("Leaving~");
-            await Context.Guild.LeaveAsync();
-        }
-
+    {       
         [Command("say"), Alias("s")]
         [Remarks("Repeats what the user says.")]
         [MinPermissions(AccessLevel.ServerAdmin)]
---
+++ GynBot/Modules/Owner/OwnerModule.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using GynBot.Common.Attributes;
+using GynBot.Common.Enums;
+using Discord.WebSocket;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace GynBot.Modules.Public
+{
+    [Name("Owner Commands")]
+    public class OwnerModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("leave")]
+        [Remarks("Instructs the bot to leave this Guild.")]
+        [MinPermissions(AccessLevel.ServerOwner)]
+        public async Task Leave()
+        {
+            if (Context.Guild == null) { await ReplyAsync("This command can only be ran in your guild."); return; }
+            await ReplyAsync("Leaving~");
+            await Context.Guild.LeaveAsync();
+        }
+
+        [Command("echo")]
+        [Summary("Echo's input into a specified channel.")]
+        [Remarks("say #general I am alive!")]
+        [MinPermissions(AccessLevel.ServerOwner)]
+        public void Say([Summary("Target channel")] ITextChannel channel, [Remainder, Summary("Text to echo")] string text)
+        {
+            (Context.Client.GetChannel(channel.Id) as SocketTextChannel)?.SendMessageAsync(text);
+        }
+
+        [Command("powerdown"), Alias("pd")]
+        [Summary("Terminates the bot application")]
+        [Remarks("powerdown")]
+        [MinPermissions(AccessLevel.BotOwner)]
+        public async Task PowerdownAsync()
+        {
+            await ReplyAsync("Powering down!").ConfigureAwait(false);
+            await Context.Client.DisconnectAsync().ConfigureAwait(false);
+            await Task.Delay(1500).ConfigureAwait(false);
+        }
+
+        [Command("getinvite")]
+        [Summary("Makes an invite to the specified guild")]
+        [Remarks("getinvite 123456789987654321")]
+        [MinPermissions(AccessLevel.BotOwner)]
+        public async Task GetInviteAsync([Summary("Target guild id")]ulong guild)
+        {
+            var channel = Context.Client.GetChannel((Context.Client.GetGuild(guild)).DefaultChannel.Id);
+            var invite = await (channel as SocketGuildChannel).CreateInviteAsync();
+            await ReplyAsync(invite.Url);
+        } 
+    }
+}
