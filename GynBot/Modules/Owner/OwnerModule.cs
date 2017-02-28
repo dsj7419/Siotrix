4d6911f3fd3f17abef026e52ce45a7a6e69e5334
Dan Johnson <dsj7419@gmail.com>
2017-02-27 21:33:47 -0800

    Added a guildlist command to list all guilds the bot is in (bot owner only)

--- GynBot/Modules/Owner/OwnerModule.cs
+++ GynBot/Modules/Owner/OwnerModule.cs
@@ -5,9 +5,11 @@ using Discord;
 using Discord.Commands;
 using GynBot.Common.Attributes;
 using GynBot.Common.Enums;
+using GynBot.Common.Types;
 using Discord.WebSocket;
 using System.Runtime.InteropServices;
 using System.Diagnostics;
+using System.Text;
 
 namespace GynBot.Modules.Public
 {
@@ -54,6 +56,40 @@ namespace GynBot.Modules.Public
             var channel = Context.Client.GetChannel((Context.Client.GetGuild(guild)).DefaultChannel.Id);
             var invite = await (channel as SocketGuildChannel).CreateInviteAsync();
             await ReplyAsync(invite.Url);
+        }
+
+        [Command("guildlist")]
+        [Summary("Lists guilds and owner for that guild")]
+        [Remarks("guildlist")]
+        [MinPermissions(AccessLevel.BotOwner)]
+        public async Task GuildListAsync()
+        {
+            var cl = Context.Client as DiscordSocketClient;
+            StringBuilder sb = new StringBuilder();
+            var Color = new Color(114, 137, 218);
+            var prefix = Configuration.Load().Prefix;
+
+            foreach (SocketGuild guild in cl.Guilds)
+            {
+                sb.AppendLine($"**Guild Name: **{guild.Name}\n**Guild ID: ** {guild.Id}\n**Guild Owner: **{guild.Owner} || {guild.OwnerId}\n");
+            }
+            EmbedBuilder builder = new EmbedBuilder()
+                .WithAuthor(new EmbedAuthorBuilder()
+                .WithIconUrl("http://cdn.mysitemyway.com/etc-mysitemyway/icons/legacy-previews/icons-256/blue-jelly-icons-alphanumeric/069500-blue-jelly-icon-alphanumeric-information1.png")
+                .WithName("GynBot - A multi-purpose bot with a single global purpose")
+                .WithUrl("https://discord.gg/RMUPGSf"))
+            .WithColor(Color)
+            .WithThumbnailUrl("https://s-media-cache-ak0.pinimg.com/564x/b5/a9/30/b5a930c07975d0935afbe210363edcde.jpg")
+            .WithTitle("Information Sheet")
+            .WithDescription($"Have Gynbot join your server! Use the command {Format.Bold($"{prefix}invite")} to see how!")
+            .WithDescription(sb.ToString())
+            .WithFooter(new EmbedFooterBuilder()
+                .WithIconUrl("http://www.supagrowth.com/img/PBN-hunter-icon.ico")
+                .WithText("Holding down the fort since 2017."))
+            .WithTimestamp(DateTime.UtcNow);
+
+            await ReplyAsync("", embed: builder);
+
         }
 
         [Command("broadcast")]
