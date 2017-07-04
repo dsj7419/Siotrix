1351ee63ee17d646742a8c82bd38ddd02357990b
upworkfriend <fornewworld@yahoo.com>
2017-07-04 07:04:32 -0700

    7/4

--- src/Siotrix.Discord.Moderation/Services/WarningService.cs
+++ src/Siotrix.Discord.Moderation/Services/WarningService.cs
@@ -16,6 +16,7 @@ namespace Siotrix.Discord.Moderation
         private int count = 0;
         private IUserMessage log_msg;
         private EmbedBuilder builder = null;
+        string bad_words = null;
 
         public WarningService(DiscordSocketClient client)
         {
@@ -41,25 +42,39 @@ namespace Siotrix.Discord.Moderation
             var dictionary = LogChannelExtensions.GetFilterWords(context.Guild.Id.ToLong());
             string[] words = msg.Content.Split(' ');
             LogChannelExtensions.IsUsableLogChannel(context.Guild.Id.ToLong());
-            var channel = context.Guild.GetChannel(LogChannelExtensions.logchannel_id.ToUlong()) as ISocketMessageChannel;
             var badword = LogChannelExtensions.ParseMessages(words, dictionary);
             if (badword != null)
             {
                 count++;
+                bad_words += badword + " ";
                 var warn_count = GetWarnValue(context.Guild.Id.ToLong(), 1);
                 var warn_mute_time = GetWarnValue(context.Guild.Id.ToLong(), 2);
-                var log_channel = _client.GetChannel(LogChannelExtensions.logchannel_id.ToUlong()) as ISocketMessageChannel;
+                var mod_channel = context.Guild.GetChannel(LogChannelExtensions.modlogchannel_id.ToUlong()) as ISocketMessageChannel;
 
-                if (count == warn_count)
+                //if (count == warn_count)
+                //{
+                //    builder = GetBuilder(context, warn_count, warn_count);
+                //    log_msg = await MessageExtensions.SendMessageSafeAsync(mod_channel, "", false, builder.Build());
+                //    return;
+                //}
+                //else if (count > warn_count)
+                //{
+                //    await MuteWarnUser(context.User as IGuildUser, warn_mute_time, context);
+                //    builder = GetBuilder(context, warn_count, count);
+                //    await log_msg.ModifyAsync(x => { x.Embed = builder.Build(); });
+                //    return;
+                //}
+                if(count == 1)
                 {
-                    builder = GetBuilder(context, warn_count, warn_count);
-                    log_msg = await MessageExtensions.SendMessageSafeAsync(log_channel, "", false, builder.Build());
+                    builder = GetBuilder(context, 1, badword);
+                    log_msg = await MessageExtensions.SendMessageSafeAsync(mod_channel, "", false, builder.Build());
                     return;
                 }
-                else if (count > warn_count)
+                else
                 {
-                    await MuteWarnUser(context.User as IGuildUser, warn_mute_time, context);
-                    builder = GetBuilder(context, warn_count, count);
+                    if(count > warn_count)
+                        await MuteWarnUser(context.User as IGuildUser, warn_mute_time, context);
+                    builder = GetBuilder(context, count, bad_words);
                     await log_msg.ModifyAsync(x => { x.Embed = builder.Build(); });
                     return;
                 }
@@ -106,7 +121,7 @@ namespace Siotrix.Discord.Moderation
             }
         }
 
-        private EmbedBuilder GetBuilder(SocketCommandContext context, int set_warn_count, long all_warn_count)
+        private EmbedBuilder GetBuilder(SocketCommandContext context, int warn_count, string badword)
         {
             string value = null;
             string g_icon_url = GuildEmbedIconUrl.GetGuildIconUrl(context);
@@ -127,16 +142,50 @@ namespace Siotrix.Discord.Moderation
                 .WithText(g_footer[1]))
                 .WithTimestamp(DateTime.UtcNow);
 
-            value = "**Warnings Setting Limit** : " + set_warn_count.ToString() + "\n" +
-                            "**Number of Warnings founded** : " + all_warn_count.ToString() + "\n" +
-                            "**Note** : *" + context.User.Mention + "* number of warnings exceed!";
+            value = context.User.Mention + " has been issues **" + warn_count.ToString() + "** warning points for breaking filter rule\n" +
+              "Reason : use of the words ***" + badword + "***\n";
+
             embed
                 .AddField(x =>
                 {
-                    x.Name = "Violation";
+                    x.Name = "WARNING";
                     x.Value = value;
                 });
             return embed;
         }
+
+        //private EmbedBuilder GetBuilder(SocketCommandContext context, int set_warn_count, long all_warn_count)
+        //{
+        //    string value = null;
+        //    string g_icon_url = GuildEmbedIconUrl.GetGuildIconUrl(context);
+        //    string g_name = GuildEmbedName.GetGuildName(context);
+        //    string g_url = GuildEmbedUrl.GetGuildUrl(context);
+        //    string g_thumbnail = GuildEmbedThumbnail.GetGuildThumbNail(context);
+        //    string[] g_footer = GuildEmbedFooter.GetGuildFooter(context);
+        //    string g_prefix = PrefixExtensions.GetGuildPrefix(context);
+        //    var embed = new EmbedBuilder()
+        //        .WithAuthor(new EmbedAuthorBuilder()
+        //        .WithIconUrl(g_icon_url)
+        //        .WithName(g_name)
+        //        .WithUrl(g_url))
+        //        .WithColor(new Color(255, 0, 0))
+        //        .WithThumbnailUrl(g_thumbnail)
+        //        .WithFooter(new EmbedFooterBuilder()
+        //        .WithIconUrl(g_footer[0])
+        //        .WithText(g_footer[1]))
+        //        .WithTimestamp(DateTime.UtcNow);
+
+        //    value = "**Warnings Setting Limit** : " + set_warn_count.ToString() + "\n" +
+        //                    "**Number of Warnings founded** : " + all_warn_count.ToString() + "\n" +
+        //                    "**Note** : *" + context.User.Mention + "* number of warnings exceed!";
+
+        //    embed
+        //        .AddField(x =>
+        //        {
+        //            x.Name = "WARNING";
+        //            x.Value = value;
+        //        });
+        //    return embed;
+        //}
     }
 }
